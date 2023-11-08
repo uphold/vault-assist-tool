d2117c6181dd46b882d075066e0b0b88ea71e5c2
chriselly <chris.elly@uphold.com>
2023-11-08 17:58:11 -0600

    Improve form toast error handling

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -14,7 +14,6 @@ export const useErrors = (
 ) => {
   const errorsRef = useRef();
   const previousRef = useRef();
-  const previousError = useRef();
   const values = useWatch({ control });
 
   useEffect(() => {
@@ -24,18 +23,6 @@ export const useErrors = (
 
     if (!previous || !errorsRef.current) {
       return;
-    }
-
-    // Toast errors
-    if (formErrors) {
-      Object.entries(formErrors).forEach(([, error]) => {
-        const previousErr = previousError.current;
-
-        if (error.message !== previousErr) {
-          Toaster.showError(error.message);
-          previousError.current = error.message;
-        }
-      });
     }
 
     Object.entries(errorsRef.current).forEach(([field, error]) => {
