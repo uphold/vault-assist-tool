17ba8a99253e53b719258ba0d4a42f5802e91241
chriselly <chris.elly@uphold.com>
2024-09-12 10:28:06 -0500

    Add COREUM for testing

--- src/lib/svgs/tokens/index.js
+++ src/lib/svgs/tokens/index.js
@@ -1,8 +1,11 @@
+import CORE from './CORE.svg';
 import Default from './Default.svg';
 import SOLO from './SOLO.svg';
 import XRP from './XRP.svg';
 
 export default {
+  CORE,
+  CORECOIN: CORE,
   Default,
   SOLO,
   SOLOCOIN: SOLO,
